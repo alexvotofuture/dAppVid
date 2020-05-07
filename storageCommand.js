6fecbaf41a6ecf24525040110c092bf3a7bbd618
Alessandro Voto <alex.voto@gmail.com>
2020-05-07 00:21:56 -0700

    Update storageCommand.js

--- storageCommand.js
+++ storageCommand.js
@@ -17,8 +17,8 @@ for await (const result of node.add("Here's a simple test")) {
 
 ~~~~~~~~~~~~~~~~~~~~~~
 //To retrieve
-var chunks = []
-for await (const chunk of node.cat("QmXzCSfyPdmSiysB1R9Lc7Jf4kbNvEvcZU7T79UB7Z5qM6")) {
-  chunks.push(chunk)
+var receivedBuffer = []
+for await (const chunk of node.cat("QmdrRpsuyRWyAVwtkNLViAENqEbB8iC6szFzbwxGuhSVWB")) {
+  receivedBuffer.push(chunk)
 }
-console.log(chunks[0].toString())
+console.log(receivedBuffer.join().toString());
