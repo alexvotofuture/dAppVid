037028e2ab2460d8ec66b2c1d2aed7cefe50b3a6
Alessandro Voto <alex.voto@gmail.com>
2020-05-07 01:17:18 -0700

    Update storageCommand.js

--- storageCommand.js
+++ storageCommand.js
@@ -1,16 +1,16 @@
 //To add a file to IPFS and return the CID
-async function addContentToIPFS(){
-    for await (const result of node.add(contentToAdd)) {
-        if (result && result.cid) {  
-            return(result.path); //return the IPFS hash of the content we added
-            //console.log("CID of content added to IPFS" + result.path)
-            //console.log('successfully stored', file.cid) //not needed
+async function addContentToIPFS(){ //create an async function
+    for await (const result of node.add(contentToAdd)) { //wait for the result of adding the content, then save the result as "result"
+        if (result && result.cid) { //If we successfully reveived a result and the result has a CID available
+            return(result.path); //return the IPFS CID of the content we added
+            //console.log("CID of content added to IPFS" + result.path) //for debugging
+            //console.log('successfully stored', file.cid) //not needed - for if we want that CID within the promise
         }
     }
 }
-const contentToAdd = "This is a test";
-addContentToIPFS(contentToAdd).then(function(hash){
-console.log(hash);
+const contentToAdd = "This is a test"; //Some dummy text. Replace with what you want to store.
+addContentToIPFS(contentToAdd).then(function(hash){ //Input the content to add, run the async function, and when we get a response push the CID/hash into the following function 
+console.log(hash); //share the hash in the console.
 });
 
 
