b425ae6a0f8db37c8154088fbd1e33ec434d778f
Alessandro Voto <alex.voto@gmail.com>
2020-05-06 23:30:07 -0700

    Update storageCommand.js

--- storageCommand.js
+++ storageCommand.js
@@ -1,6 +1,6 @@
 //Place this into the JS console on the html webpage to send a string to IPFS
 async function store () {
-    const toStore = "fuckmynutzo"
+    const toStore = "This is a test, and only a test!"
 
     for await (const file of node.add(toStore)) {
       if (file && file.cid) {
